6bd3fa436e05f9215beda992b5aaecda12b3fe31
Ebenezer Quayson <2425401402@gctu.live.edu>
2026-02-08 01:31:22 +0000

    Add Supabase client and update .gitignore for local env files

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,15 @@
 import { Text, View } from 'react-native';
+import { use, useEffect } from 'react';
+import { supabase } from '@/lib/supabase';
+
 
 export default function Index() {
+useEffect(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    console.log('Session:', data.session)
+  })
+}, [])
+
   return (
     <View
       style={{
